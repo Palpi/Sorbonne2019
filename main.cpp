a945652471210675798c276a9e1730a4718a0c52
Palpi <natula13@gmail.com>
2019-03-16 12:38:13 +0100

    example lecture 4

--- main.cpp
+++ main.cpp
@@ -9,33 +9,33 @@ int main()
 {
 
     // Lecture 2
-    cout << "factorial of 5: " << factorial(5) << endl;
-    for (int i=0; i<10; i++)
-    {
-        cout << (i+1) << ". element in Fibonacci sequence: " << Fibonacci(i+1) << endl;
-    }
+    // cout << "factorial of 5: " << factorial(5) << endl;
+    // for (int i=0; i<10; i++)
+    // {
+    //     cout << (i+1) << ". element in Fibonacci sequence: " << Fibonacci(i+1) << endl;
+    // }
     
-    cout << "Is 5 even?" << isEven(5) << endl;
-    basicOperations();
-    swapAB();
+    // cout << "Is 5 even?" << isEven(5) << endl;
+    // basicOperations();
+    // swapAB();
 
-    int egArrInt[5] = {45, 12023, 62, 923, -5}; 
-    cout << "Max of {45, 12023, 62, 923, -5} is: " << max(egArrInt, 5) << endl;
+    // int egArrInt[5] = {45, 12023, 62, 923, -5}; 
+    // cout << "Max of {45, 12023, 62, 923, -5} is: " << max(egArrInt, 5) << endl;
 
-    double egArrDbl[5] = {45.2, 120, 62.12, 93.3, -5.3}; 
-    cout << "Mean and variance of {45.2, 120, 62.12, 93.3, -5.3} are: " << endl;
-    meanAndVariance(egArrDbl, 5);
+    // double egArrDbl[5] = {45.2, 120, 62.12, 93.3, -5.3}; 
+    // cout << "Mean and variance of {45.2, 120, 62.12, 93.3, -5.3} are: " << endl;
+    // meanAndVariance(egArrDbl, 5);
     // end of Lecture 2
 
     // Lecture 3
-    int testNr;
-    string ans;
-    cout << "Enter a number to check if it is prime or not" << endl;
-    cin >> testNr;
-    ans = isPrime(testNr) ? " is prime " : " is not prime ";
-    cout << testNr << ans << endl;
+    // int testNr;
+    // string ans;
+    // cout << "Enter a number to check if it is prime or not" << endl;
+    // cin >> testNr;
+    // ans = isPrime(testNr) ? " is prime " : " is not prime ";
+    // cout << testNr << ans << endl;
 
-    quadraticEquation();
+//    quadraticEquation();
     HeronFormula();
 
 system("pause");
