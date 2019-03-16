26a206f9e3c0e5bf4af5e15821ecf6d6ebc86852
Palpi <natula13@gmail.com>
2019-03-16 17:07:42 +0100

    lecture 4 all

--- Lecture4.h
+++ Lecture4.h
@@ -112,5 +112,74 @@ void guessRandom() {
 
 }
 
+double providePi(long precision) {
+
+    int i;
+    double x, y, pi = 0, c;
+
+    srand((unsigned) time(0));
+
+    for (i = 0; i < precision; ++i) {
+
+        x = (double) rand() / RAND_MAX;
+        y = (double) rand() / RAND_MAX;
+        c = x * x + y * y;
+        if (c <= 1) {
+            pi++;
+        }
+    }
+
+    pi /= precision;
+    pi *= 4;
+
+    return pi;
+}
+
+double boxMuller(double pmean, double pvar){
+
+    double x, y, s, z, p;
+
+    do {
+        x = ((double) rand() * 2 / RAND_MAX) - 1;
+        y = ((double) rand() * 2 / RAND_MAX) - 1;
+        s = x * x + y * y;
+    } while (s > 1);
+
+    z = x * sqrt(-2 * log(s) / s);
+    p = sqrt(pvar) * z + pmean;
+
+    return p;
+}
+
+
+void checkBoxMullerFunction() {
+
+    int i, iter = 10000;
+    double p, desiredMean, desiredVar, sum = 0, var = 0;
+
+    srand((unsigned)time(0));
+
+    cout << "Please provide desired mean of parametrised normal distribution: ";
+    cin >> desiredMean;
+    cout << "Please provide desired variance of parametrised normal distribution: ";
+    cin >>  desiredVar;
+
+    for (i = 0; i < iter; ++i) 
+    {
+        p = boxMuller(desiredMean, desiredVar);
+        sum += p;
+        var += p * p;
+    }
+
+    cout << "Mean is supposed to be: " << desiredMean << endl;
+    cout << "..and the sample mean is: " << sum / iter << endl;
+
+    var /= iter;
+    var = var - sum * sum / (iter * iter);
+
+    cout << "Variance is supposed to be: " << desiredVar << endl;
+    cout << "..and the sample variance is: " << var << endl;
+
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -10,40 +10,42 @@ int main()
 {
 
     // Lecture 2
-    // cout << "factorial of 5: " << factorial(5) << endl;
-    // for (int i=0; i<10; i++)
-    // {
-    //     cout << (i+1) << ". element in Fibonacci sequence: " << Fibonacci(i+1) << endl;
-    // }
+    cout << "factorial of 5: " << factorial(5) << endl;
+    for (int i=0; i<10; i++)
+    {
+        cout << (i+1) << ". element in Fibonacci sequence: " << Fibonacci(i+1) << endl;
+    }
     
-    // cout << "Is 5 even?" << isEven(5) << endl;
-    // basicOperations();
-    // swapAB();
+    cout << "Is 5 even?" << isEven(5) << endl;
+    basicOperations();
+    swapAB();
 
-    // int egArrInt[5] = {45, 12023, 62, 923, -5}; 
-    // cout << "Max of {45, 12023, 62, 923, -5} is: " << max(egArrInt, 5) << endl;
+    int egArrInt[5] = {45, 12023, 62, 923, -5}; 
+    cout << "Max of {45, 12023, 62, 923, -5} is: " << max(egArrInt, 5) << endl;
 
-    // double egArrDbl[5] = {45.2, 120, 62.12, 93.3, -5.3}; 
-    // cout << "Mean and variance of {45.2, 120, 62.12, 93.3, -5.3} are: " << endl;
-    // meanAndVariance(egArrDbl, 5);
+    double egArrDbl[5] = {45.2, 120, 62.12, 93.3, -5.3}; 
+    cout << "Mean and variance of {45.2, 120, 62.12, 93.3, -5.3} are: " << endl;
+    meanAndVariance(egArrDbl, 5);
     // end of Lecture 2
 
     // Lecture 3
-    // int testNr;
-    // string ans;
-    // cout << "Enter a number to check if it is prime or not" << endl;
-    // cin >> testNr;
-    // ans = isPrime(testNr) ? " is prime " : " is not prime ";
-    // cout << testNr << ans << endl;
-
-//    quadraticEquation();
- //   HeronFormula();
-
+    int testNr;
+    string ans;
+    cout << "Enter a number to check if it is prime or not" << endl;
+    cin >> testNr;
+    ans = isPrime(testNr) ? " is prime " : " is not prime ";
+    cout << testNr << ans << endl;
+    quadraticEquation();
+    HeronFormula();
+    // end of Lecture 3
 
     // Lecture 4
-    // generateLCG();
-    //guessRandom();
+    generateLCG();
+    guessRandom();
     generateRand();
+    cout << "Number pi is approximately equal to: " << providePi(1000000) << endl;
+    checkBoxMullerFunction();
+    // end of Lecture 4
 
 system("pause");
 
