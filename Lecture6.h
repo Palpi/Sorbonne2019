2264f6805c859b35ac1e54fa35cd4aeebddc787b
Palpi <natula13@gmail.com>
2019-03-30 23:15:59 +0100

    file stream

---
+++ Lecture6.h
@@ -0,0 +1,19 @@
+#ifndef LECTURE6_H
+#define LECTURE6_H
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void writingToFile()
+{
+    ofstream myfile;
+    myfile.open ("output file.txt");
+    myfile << "The force is strong with you.\n";
+    myfile << "A powerful Sith you will become.\n";
+    myfile.close();
+}
+
+
+#endif
